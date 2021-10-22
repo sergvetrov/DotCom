76388ed277ba863b47d256812262d52e9f56e70f
Serhii Vietrov <sb.vetrov@gmail.com>
2021-10-22 17:34:24 +0300

    changed DotComBust

--- src/com/company/SimpleDotComTestDrive.java
+++ src/com/company/SimpleDotComTestDrive.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class SimpleDotComTestDrive {
     public static void main(String[] args) {
-        SimpleDotCom dot = new SimpleDotCom();
+        DotCom dot = new DotCom();
         int[] locations = {2, 3, 4};
         dot.setLocationCells(locations);
         String userGuess = "2";
